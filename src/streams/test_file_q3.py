f72b052087a5948c356531f292a546944ccb6018
Gessyca-Oliveira <gessyca.estudos@gmail.com>
2026-04-15 21:24:26 -0300

    delete: unused files

--- src/streams/test_file_q3.py
+++
@@ -1,9 +0,0 @@
-from streams.animal_input_stream import AnimalInputStream
-
-with open("animais.bin", "rb") as f:
-    stream = AnimalInputStream(f)
-    animais = stream.read_animais()
-
-print("Animais lidos do arquivo:")
-for a in animais:
-    print(a.__class__.__name__, a.nome, a.idade)
